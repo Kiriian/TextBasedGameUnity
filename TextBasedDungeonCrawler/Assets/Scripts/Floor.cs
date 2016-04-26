25cfd4459769eb7c9a91a93d7beef46bb725b31d
Nicolai Bonderup <piskebenet@gmail.com>
2016-04-26 12:37:57 +0200

    Fix
    Re-enabled the default first floor. If you want to try working with the
    generated floors, comment out the two lines that are commented out in
    Floor 1 in the Floor script.

--- TextBasedDungeonCrawler/Assets/Scripts/Floor.cs
+++ TextBasedDungeonCrawler/Assets/Scripts/Floor.cs
@@ -10,63 +10,65 @@ public class Floor : ScriptableObject {
 
 	public Room[,] Floor1(){
 
-//		// Temporary for testing.
-//		mon = ScriptableObject.CreateInstance<Monster>();
-//		mon.InitSpider ();
-//
-//		boss = ScriptableObject.CreateInstance<Boss>();
-//		boss.InitTestBoss ();
-//
-//		Direction north = ScriptableObject.CreateInstance<Direction> ();
-//		north.DirectionName = "North";
-//		Direction south = ScriptableObject.CreateInstance<Direction> ();
-//		south.DirectionName = "South";
-//		Direction west = ScriptableObject.CreateInstance<Direction> ();
-//		west.DirectionName = "West";
-//		Direction east = ScriptableObject.CreateInstance<Direction> ();
-//		east.DirectionName = "East";
-//
-//
-//		Room r1 = ScriptableObject.CreateInstance<Room>();
-//		r1.Description = "Starting room";
-//		HealingPotion pot = ScriptableObject.CreateInstance<HealingPotion>();
-//		pot.name = "Healing Potion";
-//		r1.Items.Add (pot);
-//		r1.Directions.Add (south);
-//		r1.RoomMonster = mon;
-//
-//		Room r2 = ScriptableObject.CreateInstance<Room>();
-//		r2.Description = "A Narrow Hallway with some Unburned Torchs";
-//		r2.Directions.Add (south);
-//		r2.Directions.Add (north);
-//		r2.Directions.Add (east);
-//
-//		Room r3 =  ScriptableObject.CreateInstance<Room>();
-//		r3.Description = "A Long Room with Cages Dangeling from the Celling";
-//		r3.Directions.Add (west);
-//
-//		Room r4 =  ScriptableObject.CreateInstance<Room>();
-//		r4.Description = "A Room with Alchemy Workbenches";
-//		r4.Directions.Add (north);
-//		r4.Directions.Add (south);
-//
-//		Room r5 =  ScriptableObject.CreateInstance<Room>();
-//		r5.Description = "A Big Room with a Giant Golem in the Middle";
-//		r5.EntranceToNextFloor = true;
-//		r5.Directions.Add (north);
-//		r5.RoomBoss = boss;
-//
-//		roomArray2d = new Room[4, 2];
-//
-//		roomArray2d [0, 0] = r1;
-//		roomArray2d [1, 0] = r2;
-//		roomArray2d [1, 1] = r3;
-//		roomArray2d [2, 0] = r4;
-//		roomArray2d [3, 0] = r5;
+		// Temporary for testing.
+		mon = ScriptableObject.CreateInstance<Monster>();
+		mon.InitSpider ();
+
+		boss = ScriptableObject.CreateInstance<Boss>();
+		boss.InitTestBoss ();
+
+		Direction north = ScriptableObject.CreateInstance<Direction> ();
+		north.DirectionName = "North";
+		Direction south = ScriptableObject.CreateInstance<Direction> ();
+		south.DirectionName = "South";
+		Direction west = ScriptableObject.CreateInstance<Direction> ();
+		west.DirectionName = "West";
+		Direction east = ScriptableObject.CreateInstance<Direction> ();
+		east.DirectionName = "East";
 
 
-		FloorGenerator fg = ScriptableObject.CreateInstance<FloorGenerator> ();
-		roomArray2d = fg.GenerateFloor ();
+		Room r1 = ScriptableObject.CreateInstance<Room>();
+		r1.Description = "Starting room";
+		HealingPotion pot = ScriptableObject.CreateInstance<HealingPotion>();
+		pot.name = "Healing Potion";
+		r1.Items.Add (pot);
+		r1.Directions.Add (south);
+		r1.RoomMonster = mon;
+
+		Room r2 = ScriptableObject.CreateInstance<Room>();
+		r2.Description = "A Narrow Hallway with some Unburned Torchs";
+		r2.Directions.Add (south);
+		r2.Directions.Add (north);
+		r2.Directions.Add (east);
+
+		Room r3 =  ScriptableObject.CreateInstance<Room>();
+		r3.Description = "A Long Room with Cages Dangeling from the Celling";
+		r3.Directions.Add (west);
+
+		Room r4 =  ScriptableObject.CreateInstance<Room>();
+		r4.Description = "A Room with Alchemy Workbenches";
+		r4.Directions.Add (north);
+		r4.Directions.Add (south);
+
+		Room r5 =  ScriptableObject.CreateInstance<Room>();
+		r5.Description = "A Big Room with a Giant Golem in the Middle";
+		r5.EntranceToNextFloor = true;
+		r5.Directions.Add (north);
+		r5.RoomBoss = boss;
+
+		roomArray2d = new Room[4, 2];
+
+		roomArray2d [0, 0] = r1;
+		roomArray2d [1, 0] = r2;
+		roomArray2d [1, 1] = r3;
+		roomArray2d [2, 0] = r4;
+		roomArray2d [3, 0] = r5;
+
+		// Comment out all code above and uncomment the two following lines to enable floor generation.
+
+
+//		FloorGenerator fg = ScriptableObject.CreateInstance<FloorGenerator> ();
+//		roomArray2d = fg.GenerateFloor ();
 
 		return roomArray2d;
 	}
